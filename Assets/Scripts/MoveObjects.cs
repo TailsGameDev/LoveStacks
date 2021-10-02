b1a8ad0e2ac6ca428dbbe10ab214d9e5884b8706
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-02 10:49:08 -0300

    gitignore update and replace files

---
+++ Assets/Scripts/MoveObjects.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjects : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
