e1dfedfdbb5262ca9d99c13965b443b4f47bcb63
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-04 13:49:14 -0300

    Tutorials implemented

---
+++ Assets/Scripts/ButtonInteractable.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInteractable : Interactable
+{
+    private enum ButtonType
+    {
+        OK,
+        QUIT_TUTORIAL,
+    }
+
+    [SerializeField]
+    private GameObject tutorialToQuit = null;
+
+    [SerializeField]
+    private ButtonType buttonType = 0;
+
+    [SerializeField]
+    private GameObject explosionVFX = null;
+
+    [SerializeField]
+    private Transform spawnPointForExplosionVFX = null;
+
+    [SerializeField]
+    private UnityEvent onClick = null;
+
+    private static System.Action quitTutorial;
+    
+    private void OnEnable()
+    {
+        if (tutorialToQuit != null)
+        {
+            ButtonInteractable.quitTutorial += QuitTutorial;
+        }
+    }
+    private void OnDisable()
+    {
+        if (tutorialToQuit != null)
+        {
+            ButtonInteractable.quitTutorial -= QuitTutorial;
+        }
+
+        if (explosionVFX != null)
+        {
+            Instantiate(explosionVFX, spawnPointForExplosionVFX.position, Quaternion.identity);
+        }
+    }
+    private void QuitTutorial()
+    {
+        tutorialToQuit.SetActive(false);
+    }
+
+    public override void StartInteraction()
+    {
+        switch (buttonType)
+        {
+            case ButtonType.OK:
+                onClick?.Invoke();
+                break;
+            case ButtonType.QUIT_TUTORIAL:
+                ButtonInteractable.quitTutorial.Invoke();
+                break;
+        }
+    }
+}
